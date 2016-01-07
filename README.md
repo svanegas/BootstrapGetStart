258ef3518543bd61c83bed32191bc74fa39542ad
Carolina Penagos <carolinapenagos@henao.local>
2016-01-07 16:16:39 -0500

    template ready (create a navbar, divs for the contacts, for de conversation and for the user information), add the bower.son, install wired to inject the bower dependencies and create a basic readme

---
+++ README.md
@@ -0,0 +1,22 @@
+# Dillinger
+Is a project to learn how create good user interface using css, html and bootstrap.
+
+### Tech
+* [Twitter Bootstrap] - great UI boilerplate for modern web apps
+* [Express] - fast node.js network app framework
+* [Gulp] - the streaming build system
+* [jQuery] - duh
+
+### Install:
+
+```sh
+$ npm i -g gulp
+```
+
+```sh
+$ npm install --save gulp-install
+$ npm install --save wiredep
+$ npm install gulp-express --save-dev 
+$ npm install express --save
+$ npm install connect-livereload --save-dev
+```
--- gulpfile.js
+++ gulpfile.js
@@ -2,9 +2,22 @@
 var gulp = require('gulp');
 var path = require('path');
 var server = require('gulp-express');
+var wiredep = require('wiredep').stream;
 
 //DEFINE GLOBAL PATHS
+var config = {
+  app: 'app',
+  dist: 'dist'
+};
  
+//Inject the bower.json dependencies in index.html file
+gulp.task('wiredep', function () {
+  gulp.src( path.join(config.app, '/index.html'))
+    .pipe(wiredep(
+      //Wiredepp special configuration
+    )).pipe(gulp.dest(config.app));
+});
+
 gulp.task('watch', function () {
   gulp.watch(['./app/*.html'], server.notify);
   gulp.watch(['./app/*.js'], server.notify);
@@ -19,6 +32,7 @@ gulp.task('server', function () {
 
 gulp.task('default', [
   'server',
+  'wiredep',
   'watch'
 ]);
 

03c1ddb59922f90cf57311ac7f0f84d3b3f4f9d0
Santiago Vanegas Gil <savanegasg@gmail.com>
2016-01-12 11:40:32 -0500

    Using a gulp task to compile Less into CSS, and watch also includes less changes

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,17 @@
 //IMPORT THE REQUIRED LIBS
-var gulp = require('gulp');
-var path = require('path');
-var server = require('gulp-express');
-var wiredep = require('wiredep').stream;
+var gulp        = require('gulp');
+var server      = require('gulp-express');
+var less        = require('gulp-less');
+var path        = require('path');
+var runSequence = require('run-sequence');
+var wiredep     = require('wiredep').stream;
 
 //DEFINE GLOBAL PATHS
 var config = {
   app: 'app',
   dist: 'dist'
 };
- 
+
 //Inject the bower.json dependencies in index.html file
 gulp.task('wiredep', function () {
   gulp.src( path.join(config.app, '/index.html'))
@@ -18,16 +20,31 @@ gulp.task('wiredep', function () {
     )).pipe(gulp.dest(config.app));
 });
 
+gulp.task('less', function () {
+  return gulp.src([
+    path.join(config.app, '**/*.less'),
+    path.join('!' + config.app, '/bower_components/**/*')
+  ])
+    .pipe(less())
+    .pipe(gulp.dest(config.app));
+});
+
+var lessAndReload = function(event) {
+  runSequence('less', function() {
+    server.notify(event);
+  });
+}
+
 gulp.task('watch', function () {
   gulp.watch(['./app/*.html'], server.notify);
   gulp.watch(['./app/*.js'], server.notify);
-  gulp.watch(['./app/*.css'], server.notify);
+  gulp.watch(['./app/*.less'], lessAndReload);
   gulp.watch(['./server/**/*.js'], server.notify);
 });
 
 gulp.task('server', function () {
     server.run(['server/app.js']);
- 
+
 });
 
 gulp.task('default', [
@@ -35,4 +52,3 @@ gulp.task('default', [
   'wiredep',
   'watch'
 ]);
-
